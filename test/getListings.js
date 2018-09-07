bfa8e1a28e2850e5d5c7fb2444e5ba6b1422d618
Isaac Kang <isaac.kang@consensys.net>
2018-09-06 21:50:16 -0400

    rm extraneous pkgs

--- test/getListings.js
+++ test/getListings.js
@@ -1,35 +1,17 @@
 const test = require('tape')
-// const path = require('path')
-// const sortBy = require('lodash/fp/sortBy')
-// const low = require('lowdb')
-// const FileSync = require('lowdb/adapters/FileSync')
-// const db = low(new FileSync(path.resolve(__dirname, '../db/store.json')))
-// db.defaults({ listings: {}, applicationLogs: [], nonApplicationLogs: [] }).write()
-// const { getInDB, getListingsDB, setToDB } = require('../db')
 const TCRListings = require('../lib')
 
 test('getListings', async t => {
-  // let localCandidatesMap = getListingsDB(db)
-  // const localListings = Object.keys(localCandidatesMap).map(liHash => localCandidatesMap[liHash])
-
-  // const aLogs = getInDB(db, 'applicationLogs')
-  // const nLogs = getInDB(db, 'nonApplicationLogs')
-  // const applicationLogs = sortBy([log => log.txData.blockTimestamp], aLogs)
-  // const nonApplicationLogs = sortBy([log => log.txData.blockTimestamp], nLogs)
+  // Get logs
 
   const tcrListings = new TCRListings(
-    [],
-    [],
-    // applicationLogs,
-    // nonApplicationLogs,
+    [], // applicationLogs,
+    [], // nonApplicationLogs,
     '0xd09cc3bc67e4294c4a446d8e4a2934a921410ed7'
   )
   const listingsMap = await tcrListings.getListings()
 
   console.log('# total candidates:', listingsMap.size)
   console.log('listingsMap.toJS():', listingsMap.toJS())
-
-  // setToDB(db, 'listings', listingsMap.toJS())
-
   t.end()
 })
